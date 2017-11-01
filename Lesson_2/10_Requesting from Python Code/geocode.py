80c68627b3a33b8248e51374c297e853604d9ba7
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-11-01 16:25:11 +0800

    geocode.py bug fixed

--- Lesson_2/10_Requesting from Python Code/geocode.py
+++ Lesson_2/10_Requesting from Python Code/geocode.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3
 import httplib2
 import json
 
 def getGeocodeLocation(inputString):
     # Use Google Maps to convert a location into Latitute/Longitute coordinates
     # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-    google_api_key = "PASTE_YOUR_KEY_HERE"
+    google_api_key = "google_map_api_key_here"
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     h = httplib2.Http()
-    result = json.loads(h.request(url,'GET')[1])
+    #add decode method to avoid JSON type error
+    result = json.loads(h.request(url,'GET')[1].decode('utf-8'))
     latitude = result['results'][0]['geometry']['location']['lat']
     longitude = result['results'][0]['geometry']['location']['lng']
-    return (latitude,longitude)
+    #return (latitude,longitude)
+    print(latitude, longitude)
+    return
+
+if __name__ == '__main__':
+	getGeocodeLocation("beijing")
